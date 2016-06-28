4115eb5c479c1a32c633f617dde662fdac2cdc44
Matthew Flickinger <mflick@umich.edu>
2016-06-28 09:50:37 -0400

    Clean SMS column names

--- R/backups.R
+++ R/backups.R
@@ -16,12 +16,12 @@ list_backups <- function(dir = ifelse(.Platform$OS.type=="windows","%APPDATA%\\A
 
 read_sms_data <- function(dir = list_backups()[1], db = "3d0d7e5fb2ce288813306e4d4636395e047a3d28") {
     con <- RSQLite::dbConnect(drv=RSQLite::SQLite(), dbname=file.path(dir, db))
-    sql <- paste0("SELECT m.rowid as RowID, DATETIME(date + 978307200, 'unixepoch', 'localtime') as Date, ", 
-    	"h.id as 'Phone Number', m.service as Service, ",
-    	"CASE is_from_me WHEN 0 THEN 'Received' WHEN 1 THEN 'Sent' ELSE 'Unknown' END as Type, ",
-    	"CASE WHEN date_read > 0 THEN DATETIME(date_read + 978307200, 'unixepoch') WHEN date_delivered > 0 THEN DATETIME(date_delivered + 978307200, 'unixepoch') ELSE NULL END as 'Date Read/Sent', ",
-    	"text as Text, ma.attachment_id, a.filename, ", 
-    	"CASE a.is_outgoing WHEN 0 THEN 'Incoming' WHEN 1 THEN 'Outgoing' ELSE NULL END as AType, ",
+    sql <- paste0("SELECT m.rowid as message_id, DATETIME(date + 978307200, 'unixepoch', 'localtime') as message_date, ", 
+    	"h.id as 'from', m.service as service, ",
+    	"CASE is_from_me WHEN 0 THEN 'received' WHEN 1 THEN 'sent' ELSE 'unknown' END as type, ",
+    	"CASE WHEN date_read > 0 THEN DATETIME(date_read + 978307200, 'unixepoch') WHEN date_delivered > 0 THEN DATETIME(date_delivered + 978307200, 'unixepoch') ELSE NULL END as 'read_sent_date', ",
+    	"text as text, ma.attachment_id, a.filename, ", 
+    	"CASE a.is_outgoing WHEN 0 THEN 'incoming' WHEN 1 THEN 'outgoing' ELSE NULL END as direction, ",
     	"a.total_bytes FROM message m ",
     	"INNER JOIN handle h ON h.rowid = m.handle_id ",
     	"LEFT OUTER JOIN message_attachment_join ma ON ma.message_id=m.rowid ",
