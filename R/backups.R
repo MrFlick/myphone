8d8397d896a19224931e4d391d10aade73e9b2eb
Matthew Flickinger <mflick@umich.edu>
2016-06-28 22:58:52 -0400

    Make contacts data mergable with sms

--- R/backups.R
+++ R/backups.R
@@ -17,15 +17,16 @@ list_backups <- function(dir = ifelse(.Platform$OS.type=="windows","%APPDATA%\\A
 read_sms_data <- function(dir = list_backups()[1], db = "3d0d7e5fb2ce288813306e4d4636395e047a3d28") {
     con <- RSQLite::dbConnect(drv=RSQLite::SQLite(), dbname=file.path(dir, db))
     sql <- paste0("SELECT m.rowid as message_id, DATETIME(date + 978307200, 'unixepoch', 'localtime') as message_date, ", 
-    	"h.id as 'from', m.service as service, ",
+    	"h.id as contact, m.service as service, ",
     	"CASE is_from_me WHEN 0 THEN 'received' WHEN 1 THEN 'sent' ELSE 'unknown' END as type, ",
-    	"CASE WHEN date_read > 0 THEN DATETIME(date_read + 978307200, 'unixepoch') WHEN date_delivered > 0 THEN DATETIME(date_delivered + 978307200, 'unixepoch') ELSE NULL END as 'read_sent_date', ",
+    	"CASE WHEN date_read > 0 THEN DATETIME(date_read + 978307200, 'unixepoch') WHEN date_delivered > 0 THEN DATETIME(date_delivered + 978307200, 'unixepoch') ELSE NULL END as 'read_deliver_date', ",
     	"text as text, ma.attachment_id, a.filename, ", 
     	"CASE a.is_outgoing WHEN 0 THEN 'incoming' WHEN 1 THEN 'outgoing' ELSE NULL END as direction, ",
-    	"a.total_bytes FROM message m ",
+    	"a.total_bytes, cm.rowid as chat FROM message m ",
     	"INNER JOIN handle h ON h.rowid = m.handle_id ",
     	"LEFT OUTER JOIN message_attachment_join ma ON ma.message_id=m.rowid ",
     	"LEFT OUTER JOIN attachment a ON ma.attachment_id=a.rowid ",
+    	"LEFT OUTER JOIN chat_message_join cm ON cm.message_id = m.rowid ",
     	"ORDER BY m.rowid ASC;")
     RSQLite::dbGetQuery(conn=con, statement=sql)
 }
@@ -39,14 +40,23 @@ get_emoji <- function(x) {
 	regmatches(x, emoji)
 }
 
+sanitize_phone_number <- function(x) {
+	pn<-gsub("\\D","", x)
+	pn[nchar(pn)==10] <- paste0("1",pn[nchar(pn)==10])
+	pn[nchar(pn)==11] <- paste0("+",pn[nchar(pn)==11])
+	pn
+}
+
 read_contacts <- function(dir = list_backups()[1], db = "31bb7ba8914766d4ba40d6dfb6113c8b614be442") {
     con <- RSQLite::dbConnect(drv=RSQLite::SQLite(), dbname=file.path(dir, db))
-    sql <- paste0("select ABPerson.prefix, ABPerson.first,ABPerson.last, c.value as MobilePhone, ",
-    	"h.value as HomePhone, he.value as HomeEmail, w.value as WorkPhone, we.value as WorkEmail ",
+    sql <- paste0("select ABPerson.ROWID as contact_id, ABPerson.first,ABPerson.last, ",
+    	"v.value as email_phone, ",
+    	"CASE v.property WHEN 3 THEN 'phone' WHEN 4 THEN 'email' END as type ",
 		"from ABPerson left outer join ABMultiValue c on c.record_id = ABPerson.ROWID and c.label = 1 and c.property= 3 ",
-		"left outer join ABMultiValue h on h.record_id = ABPerson.ROWID and h.label = 2 and h.property = 3 ",
-		"left outer join ABMultiValue he on he.record_id = ABPerson.ROWID and he.label = 2 and he.property = 4 ",
-		"left outer join ABMultiValue w on w.record_id = ABPerson.ROWID and w.label = 4 and w.property = 3 ",
-		"left outer join ABMultiValue we on we.record_id = ABPerson.ROWID and we.label = 4 and we.property = 4")
-    RSQLite::dbGetQuery(conn=con, statement=sql)
+		"left outer join ABMultiValue v on v.record_id = ABPerson.ROWID ",
+		"where v.property in (3,4)")
+    dd <- RSQLite::dbGetQuery(conn=con, statement=sql)
+    dd$contact <- dd$email_phone;
+    dd$contact[dd$type=="phone"] <- sanitize_phone_number(dd$contact[dd$type=="phone"])
+    dd
 }
