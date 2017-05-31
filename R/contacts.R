7b50875981a781015460e1f8f2f26d96e51544b7
Matthew Flickinger <mflick@umich.edu>
2017-05-30 23:18:07 -0400

    Update contact extraction functions

--- R/contacts.R
+++ R/contacts.R
@@ -1,3 +1,20 @@
+#' Address book path
+#'
+#' Returns the full path to the Address Book database
+#'
+#' @param x An \code{ios_backup} object (or something that can be passed
+#'   to \code{get_backup})
+#' @return The full path to the address book database file
+#' @export
+
+path_contacts <- function(x) {
+	if (is.character(x) && length(x)==1 && file.exists(x)) {
+		path <- x
+	} else {
+		path <- backup_file_path(x, file="Library/AddressBook/AddressBook.sqlitedb", domain="HomeDomain")
+	}
+}
+
 #' Read contacts database
 #'
 #' Extract data from the Address Book in an iOS Backup.
@@ -17,11 +34,7 @@
 #' @export
 
 read_contacts <- function(x, collect=TRUE) {
-	if (is.character(x) && length(x)==1 && file.exists(x)) {
-		path <- x
-	} else {
-		path <- backup_file_path(x, file="Library/AddressBook/AddressBook.sqlitedb", domain="HomeDomain")
-	}
+	path <- path_contacts(x)
     sql <- paste0("select ABPerson.ROWID as contact_id, ABPerson.first,ABPerson.last, ",
     	"v.value as email_phone, ",
     	"CASE v.property WHEN 3 THEN 'phone' WHEN 4 THEN 'email' END as type ",
@@ -35,3 +48,125 @@ read_contacts <- function(x, collect=TRUE) {
 	}
 	dd
 }
+
+#' Read contacts
+#'
+#' Extract contacts from the Address Book in an iOS Backup.
+#'
+#' @param x An \code{ios_backup} object (or something that can be passed
+#'   to \code{get_backup}) or the path to a sqlite AddressBook database.
+#' @param collect Should dplyr results be collected before being returned.
+#' @return This will return a tibble with the contact data.
+#'   If \code{collect==FALSE}, it will be a lazy tibble.
+#'   The following columns will be included
+#' \itemize{
+#' \item{contact_id} A unique ID for each contact
+#' \item{first_name} Contact first name
+#' \item{middle_name} Contact middle name
+#' \item{last_name} Contact last name
+#' \item{organization} Contact organization
+#' \item{department} Contact department
+#' \item{jobtitle} Contact job title
+#' \item{birthday} Contact birthday
+#' \item{creation_date} Date contact created
+#'}
+#' @seealso \code{\link{read_contacts_phone_numbers}}, \code{\link{read_contacts_email_addresses}}
+#' @export
+
+read_contact_entities <- function(x, collect=TRUE) {
+	path <- path_contacts(x)
+	sql = paste0("select ABPerson.ROWID as contact_id, ",
+    	"ABPerson.first as first_name, ABPerson.last as last_name, ABPerson.middle as middle_name, ",
+     	"ABPerson.Organization as organization, ABPerson.Department as department, ",
+		"ABPerson.JobTitle as jobtitle, ",
+		"ABPerson.Birthday as birthday, ",
+		"ABPerson.CreationDate as creation_date ",
+		"FROM ABPerson")
+	dd <- dplyr::tbl(dplyr::src_sqlite(path), dplyr::sql(sql))
+	if (collect) {
+		dd <- dplyr::mutate_(dplyr::collect(dd), .dots=list(
+			creation_date = ~as.POSIXct(creation_date, origin="2001-01-01"),
+			birthday = ~as.POSIXct(as.numeric(birthday), origin="2001-01-01")
+		))
+	}
+	dd
+}
+
+#' Read contact phone numbers
+#'
+#' Extract phone numbers from the Address Book in an iOS Backup.
+#'
+#' @param x An \code{ios_backup} object (or something that can be passed
+#' to \code{get_backup}) or the path to a sqlite AddressBook database.
+#' @param collect Should dplyr results be collected before being returned.
+#' @return This will return a tibble with the contact data.
+#'   If \code{collect==FALSE}, it will be a lazy tibble.
+#'   The following columns will be included
+#' \itemize{
+#' \item{contact_id} A unique ID for each contact
+#' \item{identifier} A unique number assigned to each address 
+#'    for each contact (0 indicates "primary" address)
+#' \item{phone} Contact phone number
+#' \item{phone_type} Phone number label
+#'}
+#' @seealso \code{\link{read_contact_entities}}, \code{\link{read_contacts_email_addresses}}
+#' @export
+
+read_contacts_phone_numbers <- function(x, collect=TRUE) {
+	path <- path_contacts(x)
+	sql <- paste0("select mv.record_id as contact_id, ",
+		"mv.identifier as identifier ",
+		"mv.value as phone, ",
+		"mvl.value as phone_type ",
+		"from ABMultiValue as mv ",
+		"left join ABMultiValueLabel as mvl on mvl.ROWID = mv.label ",
+		"where mv.property=3 ",
+		"order by mv.record_id, mv.identifier")
+	dd <- dplyr::tbl(dplyr::src_sqlite(path), dplyr::sql(sql))
+	if (collect) {
+		dd <- dplyr::mutate_(dplyr::collect(dd), .dots=list(
+			phone_type = ~gsub("_\\$!<(.*)>!\\$_", "\\1", phone_type)
+		))
+	}
+	dd
+}
+
+#' Read contact email addresses
+#'
+#' Extract email addresses from the Address Book in an iOS Backup.
+#'
+#' @param x An \code{ios_backup} object (or something that can be passed
+#' to \code{get_backup}) or the path to a sqlite AddressBook database.
+#' @param collect Should dplyr results be collected before being returned.
+#' @return This will return a tibble with the contact data.
+#'   If \code{collect==FALSE}, it will be a lazy tibble.
+#'   The following columns will be included
+#' \itemize{
+#' \item{contact_id} A unique ID for each contact
+#' \item{identifier} A unique number assigned to each address 
+#'    for each contact (0 indicates "primary" address)
+#' \item{email} Contact email address
+#' \item{email_type} Email address label
+#'}
+#' @seealso \code{\link{read_contact_entities}}, \code{\link{read_contacts_phone_numbers}}
+#' @export
+
+read_contacts_email_addresses <- function(x, collect=TRUE) {
+	path <- path_contacts(x)
+	sql <- paste0("select mv.record_id as contact_id, ",
+		"mv.identifier as identifier ",
+		"mv.value as email, ",
+		"mvl.value as email_type ",
+		"from ABMultiValue as mv ",
+		"left join ABMultiValueLabel as mvl on mvl.ROWID = mv.label ",
+		"where mv.property=4 ",
+		"order by mv.record_id, mv.identifier")
+	dd <- dplyr::tbl(dplyr::src_sqlite(path), dplyr::sql(sql))
+	if (collect) {
+		dd <- dplyr::mutate_(dplyr::collect(dd), .dots=list(
+			email_type = ~gsub("_\\$!<(.*)>!\\$_", "\\1", email_type)
+		))
+	}
+	dd
+}
+
